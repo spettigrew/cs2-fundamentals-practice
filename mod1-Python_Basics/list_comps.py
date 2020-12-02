c780a6d83638f668b443656322a35f1f5fb93683
Sara Pettigrew <sarajp.ak@hotmail.com>
2020-12-01 22:20:25 -0700

    Code added to module challenges

--- mod1-Python_Basics/list_comps.py
+++ mod1-Python_Basics/list_comps.py
@@ -4,5 +4,5 @@ Use a list comprehension to create a new list called new_list out of the numbers
 
 numbers = [22.3, -184.4, 57.8, 99.6, -18.2, 84.2, 71.3]
 
-new_list = ["<write your list comprehension here>"]
+new_list = [int(number) for number in numbers if number > 0]
 print(new_list)
