3a8d26d342cf7d6098529e1bc3f1928e0b5882db
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-12-01 21:15:07 -0700

    Notes and code added to module questions

---
+++ mod1-Python_Basics/boolean.py
@@ -0,0 +1,17 @@
+""""
+Change the assignment statements below so that all of the print function calls print True.
+"""
+# change these values so that all of the print statements print True
+number = 17
+second_number = None
+first_list = [1, 0, 3]
+second_list = [0, 1]
+# only change the code above this line
+
+# all of these print statements must print True by only changing the assignment statements above
+print(number > 15)
+print(first_list)
+print(len(second_list) == 2)
+print(len(first_list) + len(second_list) == 5)
+print(first_list and first_list[0] == 1)
+print(not second_number)
---
+++ mod1-Python_Basics/import.py
@@ -0,0 +1,7 @@
+"""
+Import the "math" module. Then, print an alphabetically sorted list of all the functions available in the "math" module 
+that start with the letters "is".
+"""
+# YOUR CODE HERE
+import math
+print(dir(math))
