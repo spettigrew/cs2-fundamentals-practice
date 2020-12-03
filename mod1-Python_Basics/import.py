9399d16ff07ad4627dd91ac55eb3dc4ba0d9f925
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-12-03 16:19:00 -0700

    LIst comp setup for math function list

--- mod1-Python_Basics/import.py
+++ mod1-Python_Basics/import.py
@@ -1,7 +1,14 @@
 """
 Import the "math" module. Then, print an alphabetically sorted list of all the functions available in the "math" module 
 that start with the letters "is".
-"""
+"""                 
 # YOUR CODE HERE
 import math
-print(dir(math))
+
+# m = sorted(math)
+# def sort_list(word):
+    #lambda takes an argument and returns the length of that. Uses it to determine everything.
+    # word.sort(key = len)
+math_function = [words for words in dir(math) if words.startswith('is')]
+
+print(math_function)
