3a06296c51f78bd89312ec3b34621b25cc08aa00
Sara Pettigrew <sarajp.ak@hotmail.com>
2021-01-04 19:39:16 -0700

    Peek method added to stack in array

---
+++ mod3-Queues_Stacks/stack_dynamic-array.py
@@ -0,0 +1,32 @@
+"""
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        if len(self.data) > 0:
+            return self.data.pop()
+        return "The stack is empty"
+"""
+
+"""
+Add a peek method to the Stack class. The peek method should return the value of the top item in the stack without actually removing it from the stack.
+"""
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        if len(self.data) > 0:
+            return self.data.pop()
+        return "The stack is empty"
+    
+    def peek(self):
+        return self.items[0]
+        
