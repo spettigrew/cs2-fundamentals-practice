bc4579d9b513ba0c370c5756a3be5e0cf1e09bd3
Sara Pettigrew <sarajp.ak@hotmail.com>
2021-01-04 19:38:54 -0700

    Code added to is_empty function

---
+++ mod3-Queues_Stacks/queue_linked_lists.py
@@ -0,0 +1,77 @@
+"""
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+    def enqueue(self, item):
+        new_node = LinkedListNode(item)
+        # check if queue is empty
+        if self.rear is None:
+            self.front = new_node
+            self.rear = new_node
+        else:
+            # add new node to rear
+            self.rear.next = new_node
+            # reassign rear to new node
+            self.rear = new_node
+    def dequeue(self):
+        # check if queue is empty
+        if self.front is not None:
+            # keep copy of old front
+            old_front = self.front
+            # set new front
+            self.front = old_front.next
+
+        # check if the queue is now empty
+        if self.front is None:
+            # make sure rear is also None
+            self.rear = None
+
+        return old_front
+"""
+
+"""
+Refactor the Queue class below by adding an `is_empty` method. After writing this method, refactor your other methods to use this method in your other methods.
+"""
+class LinkedListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def is_empty(self):
+        return self.front == None
+
+    def enqueue(self, item):
+        new_node = LinkedListNode(item)
+        # check if queue is empty
+        if self.rear is None:
+            self.front = new_node
+            self.rear = new_node
+        else:
+            # add new node to rear
+            self.rear.next = new_node
+            # reassign rear to new node
+            self.rear = new_node
+
+    def dequeue(self):
+        # check if queue is empty
+        if self.front is not None:
+            # keep copy of old front
+            old_front = self.front
+            # set new front
+            self.front = old_front.next
+
+        # check if the queue is now empty
+        if self.front is None:
+            # make sure rear is also None
+            self.rear = None
+        return old_front.data
+
+    
